b06589e96c31484dc37b4acb5c83dc6159722e3b
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-06-14 22:44:22 -0400

    fix: bootstrap.yaml

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('01001', 'Proveedor A', 'A', 'No');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('01002', 'Proveedor B', 'B', 'Si');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('01003', 'Proveedor C', 'C', 'No');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('02004', 'Proveedor D', 'A', 'No');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('03005', 'Proveedor E', 'B', 'Si');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('04006', 'Proveedor F', 'C', 'No');
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES ('05007', 'Proveedor G', 'D', 'No');
